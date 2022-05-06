c52fafcfa7c657cdc5794996ab994dc889afeb89
George <george.hs@hsress.com>
2022-05-07 00:33:10 +0800

    feat: Add Custom Drawer

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import './categories_screen.dart';
 import './favorites_screen.dart';
+import '../widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({Key? key}) : super(key: key);
@@ -30,6 +31,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]['title'] as String),
       ),
+      drawer: MainDrawer(),
       body: _pages[_selectedPageIndex]['page'] as Widget,
       bottomNavigationBar: BottomNavigationBar(
           onTap: _selectPage,
---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({Key? key}) : super(key: key);
+
+  Widget _buildListTile(IconData icon, String title, VoidCallback action) =>
+      ListTile(
+          leading: Icon(
+            icon,
+            size: 26,
+          ),
+          title: Text(
+            title,
+            style: TextStyle(
+                fontFamily: 'RobotoCondensed',
+                fontSize: 24,
+                fontWeight: FontWeight.bold),
+          ),
+          onTap: action);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).colorScheme.secondary,
+            child: Text(
+              'Cooking Up!',
+              style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30,
+                  color: Theme.of(context).primaryColor),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          _buildListTile(Icons.restaurant, 'Meals', () {}),
+          _buildListTile(Icons.settings, 'Filters', () {}),
+        ],
+      ),
+    );
+  }
+}
